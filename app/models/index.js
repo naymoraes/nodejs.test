dabf56208954dbfbb4538405c3f59710f5877f5c
NAYARA MORAES <143044400+naymoraes@users.noreply.github.com>
2024-06-06 10:17:33 -0400

    test- commit

---
+++ app/models/index.js
@@ -0,0 +1,58 @@
+const { User } = require('./app/models');
+User.create({ name: 'moraes', email: 'nayarpontr@gmail.com', password: '7896396' });
+
+
+app.get('/users', (req, res) => {}); //Listar todos
+app.post('/users', (req, res) => {}); // Criar
+app.get('/users/:id', (req, res) => {}); //Buscar
+app.put('/users/:id', (req, res) => {}); //Editar
+app.delete('/users/:id', (req, res) => {}); //Deletar
+
+app.post('/register', async (req, res) => {
+  const user = await User.create(req.body);
+  res.json(user);
+});
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 &&
+      file !== basename &&
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
